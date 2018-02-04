7867ac7868dbee67cf18c7b44cffffa05fab2a2b
Alind Gupta <alindgupta@alinds-mbp.gateway.2wire.net>
2018-02-04 11:44:05 -0500

    style fixes

--- mining.m
+++ mining.m
@@ -1,22 +1,28 @@
+%%
+%  Clustering gene expression data
+%  `https://www.mathworks.com/help/bioinfo/examples/`
+%%
+
+
 cd('~/Desktop');
-f = fopen('fit2.csv');
+f = fopen('fit.csv');
 df = textscan(f, '%s %f %f %f', 'Delimiter', ',');
 fclose(f);
 
 genes = df{1};
 exprs = horzcat(df{2:4});
 
-%% gene var filter
+%% filter gene variances
 mask = genevarfilter(exprs, 'Percentile', 60);
 genes = genes(mask);
 exprs = exprs(mask,:);
 
-%% 
+%% calculate distances based on correlation
 corrDist = pdist(exprs,'corr');
 clusterTree = linkage(corrDist,'average');
 clusters = cluster(clusterTree,'maxclust',8);
 
-%%
+%% plot
 figure
 for c = 1:16
     subplot(4,4,c);
@@ -25,7 +31,7 @@ for c = 1:16
 end
 suptitle('Hierarchical Clustering of Profiles');
 
-%%
+%% k-means clustering
 rng('default');
 
 [cidx, ctrs] = kmeans(exprs,9,'dist','corr','rep',5,'disp','final');
@@ -37,7 +43,7 @@ for c = 1:16
 end
 suptitle('K-Means Clustering of Profiles');
 
-%%
+%% plot
 figure
 for c = 1:16
     subplot(4,4,c);
@@ -47,31 +53,20 @@ for c = 1:16
 end
 suptitle('K-Means Clustering of Profiles');
 
-%% 
+%% plot PCA
 [pc, zscores, pcvars] = pca(exprs);
 figure
 scatter(zscores(:,1),zscores(:,2));
-xlabel('First Principal Component');
-ylabel('Second Principal Component');
+xlabel('PC1');
+ylabel('PC2');
 title('Principal Component Scatter Plot');
 figure
 
-%%
 pcclusters = clusterdata(zscores(:,1:2),'maxclust',7,'linkage','av');
 gscatter(zscores(:,1),zscores(:,2),pcclusters)
 xlabel('First Principal Component');
 ylabel('Second Principal Component');
 title('Principal Component Scatter Plot with Colored Clusters');
-%%
+
 B = unique(pcclusters);
 out = [B,histc(pcclusters,B)]
-
-%% 
-mask = pcclusters == 3;
-genes = genes(mask);
-exprs = exprs(mask,:);
-
-
-
-
-
